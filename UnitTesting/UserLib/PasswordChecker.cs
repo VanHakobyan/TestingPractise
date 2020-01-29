39ebb10547c7ad4f6bc5be062d1b46538e19c74f
vanhakobyan <vanhakobyan1996@gmail.com>
2020-01-29 15:22:31 +0400

    user password test

---
+++ UnitTesting/UserLib/PasswordChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserLib
+{
+    public class PasswordChecker
+    {
+        public static int GetPasswordStrength(string password)
+        {
+            if (string.IsNullOrEmpty(password)) return 0;
+
+            var result = 0;
+
+            if (Math.Max(password.Length, 7) > 7) result++;
+            if (Regex.Match(password,"[a-z]").Success) result++;
+            if (Regex.Match(password,"[A-Z]").Success) result++;
+            if (Regex.Match(password,"[0-9]").Success) result++;
+            if (Regex.Match(password,"[\\!\\@\\#\\$\\%]").Success) result++;
+            
+            return result;
+        }
+    }
+}
---
+++ UnitTesting/UserLibTests/PasswordCheckerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLibTests
+{
+    [TestClass]
+    public class PasswordCheckerTests
+    {
+        private const string Password5point = "Pt231#@1";
+        private const string Password3point = "PtirPtir";
+        private const string Password5pointSpecialChar = "Ptir0@@Ptir";
+
+        [TestMethod]
+        public void GetPasswordStrength_AllChars_5Points()
+        {
+            var expected = 5;
+            var actual = PasswordChecker.GetPasswordStrength(Password5point);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetPasswordStrength_UpperCase_3Points()
+        {
+            var expected = 3;
+            var actual = PasswordChecker.GetPasswordStrength(Password3point);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetPasswordStrength_ContainsSpecialChar_5Points()
+        {
+            var expected = 5;
+            var actual = PasswordChecker.GetPasswordStrength(Password5pointSpecialChar);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
