7a447aeb82b8e8ba9e42f3a967cb8284fbcf2688
Vanik Hakobyan <vanhakobyan1996@gmail.com>
2020-02-13 23:46:57 +0400

    Update Customer.cs

--- MoqFramework/CustomerServiceLib/Customer.cs
+++ MoqFramework/CustomerServiceLib/Customer.cs
@@ -2,14 +2,8 @@
 {
     public class Customer
     {
-        public Customer(string firstName, string lastName)
-        {
-
-        }
-        public Customer(string fullName)
-        {
-
-        }
+        public Customer(string firstName, string lastName){}
+        public Customer(string fullName){}
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
